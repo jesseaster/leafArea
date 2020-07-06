4e63361f1b5859d063ca5c025b3bcf4b74bfcdf4
Jess Easter <jesseaster1@gmail.com>
2020-07-05 23:03:59 -0400

    Refined capturing the scan bed dimensions
    On the hardware side we used painters tape to mask out an exact 60 cm
    square.

--- capturePic.py
+++ capturePic.py
@@ -5,59 +5,53 @@ import os
 class CapturePic:
     def capturePic(cap):
         camera = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-        #print(camera.get(cv2.CAP_PROP_FPS))
-        #camera.set(cv2.CAP_PROP_FPS, 1)
-        print(camera.get(cv2.CAP_PROP_FPS))
         camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) #width
         camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1024) #height
         return_value, image = camera.read()
         camera.release()
         cv2.destroyAllWindows()
 
+        # Convert BGR to gray
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow("Image", image)
-        cv2.waitKey(0)
-
-
-        lower = [120, 120, 120]
-        upper = [255, 255, 255]
-
-        # create NumPy arrays from the boundaries
-        lower = np.array(lower, dtype="uint8")
-        upper = np.array(upper, dtype="uint8")
-
-        # find the colors within the specified boundaries and apply
-        # the mask
-        mask = cv2.inRange(image, lower, upper)
-        output = cv2.bitwise_and(image, image, mask=mask)
-
-        #cv2.imshow("Image", output)
-        #cv2.waitKey(0)
-
-        ret,thresh = cv2.threshold(mask, 40, 255, 0)
+        ret,thresh = cv2.threshold(gray, 160, 255, cv2.THRESH_BINARY)
+        # Otsu's thresholding after Gaussian filtering
+        blur = cv2.GaussianBlur(gray,(5,5),0)
+        ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
         contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        
-        im_rgb = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
-
-        return im_rgb
-
 
         if len(contours) != 0:
             # draw in blue the contours that were found
-            cv2.drawContours(output, contours, -1, 255, 3)
+            cv2.drawContours(image, contours, -1, 255, 3)
 
             # find the biggest countour (c) by the area
             c = max(contours, key = cv2.contourArea)
-            x,y,w,h = cv2.boundingRect(c)
+            #x,y,w,h = cv2.boundingRect(c)
 
             # draw the biggest contour (c) in green
-            cv2.rectangle(output,(x,y),(x+w,y+h),(0,255,0),2)
+            #cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+            rect = cv2.minAreaRect(c)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(image,[box],0,(0,0,255),2)
 
         # show the images
-#        cv2.imshow("Result", np.hstack([image, output]))
+        #cv2.imshow("Result", np.hstack([image, output]))
+        #cv2.imshow("gray", gray)
+        cv2.imwrite("thresh.png", image)
+        cv2.imwrite("thresh0.png", thresh)
+        #cv2.imwrite("OTSU.png", image)
+        #cv2.imwrite("OTSU0.png", th3)
+        #cv2.imshow("thresh", thresh)
+        #cv2.imshow("adaptive",th3)
+        #cv2.imshow("image", image)
 
-#        cv2.waitKey(0)
+        cv2.waitKey(0)
+        im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        return im_rgb
 
 
     # Use this function to find your camera's max resolution
@@ -79,9 +73,9 @@ class CapturePic:
         return str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 if __name__ == '__main__':
     cp = CapturePic()
-    #image = cp.capturePic()
+    image = cp.capturePic()
 
-    cp.set_res(640,	480)   # VGA
-    cp.set_res(800,	600)   # SVGA
-    cp.set_res(1280, 1024) # SXGA
+    #cp.set_res(640,	480)   # VGA
+    #cp.set_res(800,	600)   # SVGA
+    #cp.set_res(1280, 1024) # SXGA
 
